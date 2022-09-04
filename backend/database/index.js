4c99fa1c5877157cdaa09be09fa3a6c70849fbdd
Vitor Pessoti <vluiz@iblueconsulting.com.br>
2022-09-04 12:11:00 -0300

    inicializando model de alunos na configuração do sequelize

--- backend/database/index.js
+++ backend/database/index.js
@@ -4,6 +4,8 @@ const connection = new Sequelize(db);
 
 //models
 const User = require('../models/user');
+const Student = require('../models/student');
 
 User.init(connection);
+Student.init(connection);
 module.exports = connection;
